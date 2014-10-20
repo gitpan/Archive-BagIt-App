ac6db7faf896e36f2feba74bf7556a2c7448db60
Robert Schmidt <rjeschmi@gmail.com>
2014-10-20 18:17:15 +0000

    Import of RJESCHMI/Archive-BagIt-App-0.001 from CPAN.
    gitpan-cpan-distribution: Archive-BagIt-App
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         RJESCHMI/Archive-BagIt-App-0.001.tar.gz
    gitpan-cpan-author:       RJESCHMI
    gitpan-cpan-maturity:     released

---
+++ lib/Archive/Bagit/App.pm
@@ -0,0 +1,20 @@
+package Archive::BagIt::App;
+$Archive::BagIt::App::VERSION = '0.001';
+use MooseX::App;
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Archive::BagIt::App - The MooseX::App that bagit.pl runs
+
+=head1 VERSION
+
+version 0.001
+
+=cut
