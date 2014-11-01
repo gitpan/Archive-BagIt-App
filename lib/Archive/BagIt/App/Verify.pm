255e12642fd42bb8063f8e7be7a5ff42eb4ce6ec
Robert Schmidt <rjeschmi@gmail.com>
2014-11-01 22:55:13 +0000

    Import of RJESCHMI/Archive-BagIt-App-0.002-TRIAL from CPAN.
    gitpan-cpan-distribution: Archive-BagIt-App
    gitpan-cpan-version:      0.002-TRIAL
    gitpan-cpan-path:         RJESCHMI/Archive-BagIt-App-0.002-TRIAL.tar.gz
    gitpan-cpan-author:       RJESCHMI
    gitpan-cpan-maturity:     developer

--- lib/Archive/BagIt/App/Verify.pm
+++ lib/Archive/BagIt/App/Verify.pm
@@ -1,5 +1,10 @@
 package Archive::BagIt::App::Verify;
-$Archive::BagIt::App::Verify::VERSION = '0.046';
+
+use strict;
+use warnings;
+
+our $VERSION = '0.002'; # TRIAL VERSION
+
 use MooseX::App::Command;
 
 parameter 'bag_path' => (
@@ -20,6 +25,7 @@ option 'fast' => (
   isa => 'Bool',
   documentation => q[use Archive::BagIt::Fast instead...],
 );
+
 
 sub abstract {
   return 'verifies a valid bag';
@@ -50,8 +56,7 @@ sub run {
   }
 }
 
-1; 
-
+1
 
 __END__
 
@@ -61,10 +66,43 @@ __END__
 
 =head1 NAME
 
-Archive::BagIt::App::Verify - verifies a bag
+Archive::BagIt::App::Verify
 
 =head1 VERSION
 
-version 0.046
+version 0.002
+
+=for Pod::Coverage abstract run
+
+=head1 NAME
+
+Archive::BagIt::App::Verify - verifies a bag
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from the Comprehensive Perl
+Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
+site near you, or see L<https://metacpan.org/module/Archive::BagIt::App/>.
+
+=head1 SOURCE
+
+The development version is on github at L<http://github.com/rjeschmi/Archive-BagIt-App>
+and may be cloned from L<git://github.com/rjeschmi/Archive-BagIt-App.git>
+
+=head1 BUGS AND LIMITATIONS
+
+You can make new bug reports, and view existing ones, through the
+web interface at L<https://github.com/rjeschmi/Archive-BagIt-App/issues>.
+
+=head1 AUTHOR
+
+Rob Schmidt <rjeschmi@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Rob Schmidt and William Wueppelmann.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
